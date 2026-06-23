520561f16898c5fd98b77f8232604bf78f80669c
FabBeyond <redcraft42.12@gmail.com>
2026-06-23 20:29:33 +0200

    added basic resources control

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const AES_KEY = "UKu52ePUBwetZ9wNX88o54dnfKRu0T1l";
+let player_data;
+let scene_data;
 
 function openSaveFile() {
     document.getElementById("fileinput").click();
@@ -9,8 +11,12 @@ document.getElementById("fileinput").addEventListener("change", async (e) => {
     if (!file) return;
 
     const buffer = await file.arrayBuffer();
-    const saveData = decryptSaveFile(buffer);
-    console.log(saveData);
+    const save_data = decryptSaveFile(buffer);
+    player_data = save_data["playerData"];
+    scene_data = save_data["sceneData"];
+
+    console.log(save_data);
+    set_values();
 });
 
 function decryptSaveFile(arrayBuffer) {
@@ -43,14 +49,101 @@ function decryptSaveFile(arrayBuffer) {
 }
 
 function selectResourceTab(btn) {
-  document.querySelectorAll('.tab-button').forEach(b => b.classList.remove('tab-button-selected'));
-  btn.classList.add('tab-button-selected');
+  document.querySelectorAll(".tab-button").forEach(b => b.classList.remove("tab-button-selected"));
+  btn.classList.add("tab-button-selected");
 
-  const underline = document.querySelector('.tab-underline');
-  underline.style.left = btn.offsetLeft + 'px';
-  underline.style.width = btn.offsetWidth + 'px';
+  const underline = document.querySelector(".tab-underline");
+  underline.style.left = btn.offsetLeft + "px";
+  underline.style.width = btn.offsetWidth + "px";
 }
 
-window.addEventListener('load', () => {
-  selectTab(document.querySelector('.tab-button-selected'));
+window.addEventListener("load", () => {
+  selectResourceTab(document.querySelector(".tab-button-selected"));
 });
+
+function set_values() {
+    update_resources();
+    update_masks();
+    update_spools();
+}
+
+function update_masks() {
+    const images = document.querySelectorAll(".masks > img");
+    for (let i = 0; i < images.length; i++) {
+        const element = images[i];
+        if (i+1 <= player_data["maxHealth"]) {
+            element.src = "resources/mask_full.png";
+        }
+        else {
+            element.src = "resources/mask" + player_data["heartPieces"] + ".png"
+            break;
+        }
+    }
+}
+
+function update_spools() {
+    const images = document.querySelectorAll(".spools > img");
+    for (let i = 0; i < images.length; i++) {
+        const element = images[i];
+        if (i+1 <= player_data["silkMax"]-9) {
+            element.src = "resources/spool_full.png";
+        }
+        else {
+            element.src = "resources/spool" + player_data["silkSpoolParts"] + ".png"
+            break;
+        }
+    }
+}
+
+
+function change_masks(amount) {
+    player_data["heartPieces"] += amount;
+    if (player_data["heartPieces"] == 4) {
+        player_data["maxHealth"] += 1;
+        player_data["heartPieces"] = 0;
+    }
+    else if (player_data["heartPieces"] == -1) {
+        player_data["maxHealth"] -= 1;
+        player_data["heartPieces"] = 3;
+    }
+
+    if (player_data["maxHealth"] == 0) {
+        player_data["maxHealth"] = 1;
+        player_data["heartPieces"] = 0;
+    }
+    if (player_data["maxHealth"] == 10 && player_data["heartPieces"] > 0) {
+        player_data["maxHealth"] = 10;
+        player_data["heartPieces"] = 0;
+    }
+
+    update_masks();
+}
+
+function update_resources() {
+    const resources = document.querySelectorAll(".resources-entry input");
+    resources[0].value = player_data["geo"];
+    resources[1].value = player_data["ShellShards"];
+}
+
+function change_spools(amount) {
+    player_data["silkSpoolParts"] += amount;
+    if (player_data["silkSpoolParts"] == 2) {
+        player_data["silkMax"] += 1;
+        player_data["silkSpoolParts"] = 0;
+    }
+    else if (player_data["silkSpoolParts"] == -1) {
+        player_data["silkMax"] -= 1;
+        player_data["silkSpoolParts"] = 1;
+    }
+
+    if (player_data["silkMax"] == 8) {
+        player_data["silkMax"] = 9;
+        player_data["silkSpoolParts"] = 0;
+    }
+    else if (player_data["silkMax"] == 18 && player_data["silkSpoolParts"] > 0) {
+        player_data["silkMax"] = 18;
+        player_data["silkSpoolParts"] = 0;
+    }
+
+    update_spools();
+}
