6e3e69ac48d53355c4561b184b8501489b6eafd5
FabBeyond <redcraft42.12@gmail.com>
2026-06-27 22:38:44 +0200

    added booses & functionality

--- script.js
+++ script.js
@@ -195,7 +195,6 @@ function update_items() {
     if (player_data["hasQuill"]) toggle_item(document.querySelector("img[alt='quill']"));
 }
 function update_abilities() {
-    console.log(document.querySelector("img[alt='swift_step']"));
     if (player_data["hasDash"]) toggle_ability(document.querySelector("img[alt='swift_step']"));
     if (player_data["hasBrolly"]) toggle_ability(document.querySelector("img[alt='drifters_cloak']"));
     if (player_data["hasWalljump"]) toggle_ability(document.querySelector("img[alt='cling_grip']"));
@@ -206,6 +205,61 @@ function update_abilities() {
     if (player_data["hasNeedolinMemoryPowerup"]) toggle_ability(document.querySelector("img[alt='elegy']"));
     if (player_data["hasSuperJump"]) toggle_ability(document.querySelector("img[alt='silksoar']"));
     if (player_data["HasBoundCrestUpgrader"]) toggle_ability(document.querySelector("img[alt='sylphsong']"));
+}
+function update_bosses() {
+    //#region Act1
+    if (player_data["defeatedMossMother"]) toggle_ability(document.querySelector("img[alt='moss_mother']"));
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Mossbone Mother")["Record"]["Kills"] >= 3) toggle_ability(document.querySelector("img[alt='moss_mother2']"));
+    if (player_data["skullKingDefeated"]) toggle_ability(document.querySelector("img[alt='skull_tyrant']"));
+    if (player_data["skullKingKilled"]) toggle_ability(document.querySelector("img[alt='skull_tyrant2']"));
+    if (player_data["defeatedBellBeast"]) toggle_ability(document.querySelector("img[alt='bell_beast']"));
+    if (player_data["defeatedLace1"]) toggle_ability(document.querySelector("img[alt='lace']"));
+    if (player_data["defeatedSongGolem"]) toggle_ability(document.querySelector("img[alt='fourth_chorus']"));
+    if (player_data["defeatedBoneFlyerGiant"]) toggle_ability(document.querySelector("img[alt='savage_beastfly']"));
+    if (player_data["defeatedBoneFlyerGiantGolemScene"]) toggle_ability(document.querySelector("img[alt='savage_beastfly2']"));
+    if (player_data["defeatedVampireGnatBoss"]) toggle_ability(document.querySelector("img[alt='moorwing']"));
+    if (player_data["spinnerDefeated"]) toggle_ability(document.querySelector("img[alt='widow']"));
+    if (player_data["defeatedSplinterQueen"]) toggle_ability(document.querySelector("img[alt='sister_splinter']"));
+    if (player_data["defeatedPhantom"]) toggle_ability(document.querySelector("img[alt='phantom']"));
+    if (player_data["defeatedCoralDrillers"]) toggle_ability(document.querySelector("img[alt='conchflies']"));
+    if (player_data["defeatedLastJudge"]) toggle_ability(document.querySelector("img[alt='last_judge']"));
+    //#endregion
+    //#region Act2
+    if (player_data["defeatedDockForemen"]) toggle_ability(document.querySelector("img[alt='forebrothers']"));
+    if (player_data["defeatedWispPyreEffigy"]) toggle_ability(document.querySelector("img[alt='father_of_the_flame']"));
+    if (player_data["defeatedRoachkeeperChef"]) toggle_ability(document.querySelector("img[alt='lugoli']"));
+    if (player_data["DefeatedSwampShaman"]) toggle_ability(document.querySelector("img[alt='groal']"));
+    if (player_data["defeatedZapCoreEnemy"]) toggle_ability(document.querySelector("img[alt='voltwyrm']"));
+    if (player_data["defeatedCoralDrillerSolo"]) toggle_ability(document.querySelector("img[alt='conchfly']"));
+    if (player_data["defeatedFirstWeaver"]) toggle_ability(document.querySelector("img[alt='first_sinner']"));
+    if (player_data["defeatedBroodMother"]) toggle_ability(document.querySelector("img[alt='broodmother']"));
+    if (player_data["defeatedTrobbio"]) toggle_ability(document.querySelector("img[alt='trobbio']"));
+    if (player_data["defeatedCogworkDancers"]) toggle_ability(document.querySelector("img[alt='cogwork_dancers']"));
+    if (player_data["defeatedLaceTower"]) toggle_ability(document.querySelector("img[alt='lace2']"));
+    if (player_data["wardBossDefeated"]) toggle_ability(document.querySelector("img[alt='unravelled']"));
+    if (player_data["cog7_automaton_defeated"]) toggle_ability(document.querySelector("img[alt='sentinel']"));
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Garmond_Zaza") != null) toggle_ability(document.querySelector("img[alt='garmond_zaza']"));
+    //#endregion
+    //#region Act3/
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Giant Centipede") != null) toggle_ability(document.querySelector("img[alt='bell_eater']"));
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Mossbone Mother")["Record"]["Kills"] == 4) toggle_ability(document.querySelector("img[alt='moss_mother3']"));
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Mossbone Mother")["Record"]["Kills"] == 2) toggle_ability(document.querySelector("img[alt='moss_mother3']"));
+    if (player_data["defeatedAntQueen"]) toggle_ability(document.querySelector("img[alt='karmelita']"));
+    if (player_data["defeatedAntTrapper"]) toggle_ability(document.querySelector("img[alt='gurr']"));
+    if (player_data["defeatedCrowCourt"]) toggle_ability(document.querySelector("img[alt='crawfather']"));
+    if (player_data["defeatedSeth"]) toggle_ability(document.querySelector("img[alt='seth']"));
+    if (player_data["defeatedFlowerQueen"]) toggle_ability(document.querySelector("img[alt='nyleth']"));
+    if (player_data["defeatedCoralKing"]) toggle_ability(document.querySelector("img[alt='khann']"));
+    if (player_data["defeatedGreyWarrior"]) toggle_ability(document.querySelector("img[alt='watcher_at_the_edge']"));
+    if (player_data["defeatedTormentedTrobbio"]) toggle_ability(document.querySelector("img[alt='tormented_trobbio']"));
+    if (player_data["defeatedWhiteCloverstag"]) toggle_ability(document.querySelector("img[alt='palestag']"));
+    if (player_data["defeatedCloverDancers"]) toggle_ability(document.querySelector("img[alt='clover_dancers']"));
+    if (player_data["defeatedSongChevalierBoss"]) toggle_ability(document.querySelector("img[alt='pinstress']"));
+    if (player_data["BlueScientistDead"]) toggle_ability(document.querySelector("img[alt='zango']"));
+    if (player_data["garmondBlackThreadDefeated"]) toggle_ability(document.querySelector("img[alt='lost_garmond']"));
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Lost Lace") != null) toggle_ability(document.querySelector("img[alt='lost_lace']"));
+    if (player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Abyss Mass") != null) toggle_ability(document.querySelector("img[alt='summoned_saviour']"));
+    //#endregion
 }
 
 
@@ -440,6 +494,229 @@ function update_ability(obj) {
     }
 }
 
+function update_boss(obj) {
+    obj = obj.querySelector("img");
+    key = obj.alt;
+
+    //#region Act1
+    if (key == "moss_mother") {
+        player_data["defeatedMossMother"] = player_data["defeatedMossMother"] == true ? false : true;
+    }
+    else if (key == "moss_mother2") {
+        record = player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Mossbone Mother")["Record"]
+        if (record["Kills"] == 4) {
+            record["Kills"] = 2;
+        }
+        else if (record["Kills"] == 2) {
+            record["Kills"] = 4;
+        }
+    }
+    else if (key == "skull_tyrant") {
+        player_data["skullKingDefeated"] = player_data["skullKingDefeated"] == true ? false : true;
+    }
+    else if (key == "skull_tyrant2") {
+        player_data["skullKingKilled"] = player_data["skullKingKilled"] == true ? false : true;
+    }
+    else if (key == "bell_beast") {
+        player_data["defeatedBellBeast"] = player_data["defeatedBellBeast"] == true ? false : true;
+    }
+    else if (key == "lace") {
+        player_data["defeatedLace1"] = player_data["defeatedLace1"] == true ? false : true;
+    }
+    else if (key == "fourth_chorus") {
+        player_data["defeatedSongGolem"] = player_data["defeatedSongGolem"] == true ? false : true;
+    }
+    else if (key == "savage_beastfly") {
+        player_data["defeatedBoneFlyerGiant"] = player_data["defeatedBoneFlyerGiant"] == true ? false : true;
+    }
+    else if (key == "savage_beastfly2") {
+        player_data["defeatedBoneFlyerGiantGolemScene"] = player_data["defeatedBoneFlyerGiantGolemScene"] == true ? false : true;
+    }
+    else if (key == "moorwing") {
+        player_data["defeatedVampireGnatBoss"] = player_data["defeatedVampireGnatBoss"] == true ? false : true;
+    }
+    else if (key == "widow") {
+        player_data["spinnerDefeated"] = player_data["spinnerDefeated"] == true ? false : true;
+    }
+    else if (key == "sister_splinter") {
+        player_data["defeatedSplinterQueen"] = player_data["defeatedSplinterQueen"] == true ? false : true;
+    }
+    else if (key == "conchflies") {
+        player_data["defeatedCoralDrillers"] = player_data["defeatedCoralDrillers"] == true ? false : true;
+    }
+    else if (key == "phantom") {
+        player_data["defeatedPhantom"] = player_data["defeatedPhantom"] == true ? false : true;
+    }
+    else if (key == "last_judge") {
+        player_data["defeatedLastJudge"] = player_data["defeatedLastJudge"] == true ? false : true;
+    }
+    //#endregion
+    //#region Act2
+    else if (key == "forebrothers") {
+        player_data["defeatedDockForemen"] = player_data["defeatedDockForemen"] == true ? false : true;
+    }
+    else if (key == "father_of_the_flame") {
+        player_data["defeatedWispPyreEffigy"] = player_data["defeatedWispPyreEffigy"] == true ? false : true;
+    }
+    else if (key == "lugoli") {
+        player_data["defeatedRoachkeeperChef"] = player_data["defeatedRoachkeeperChef"] == true ? false : true;
+    }
+    else if (key == "groal") {
+        player_data["DefeatedSwampShaman"] = player_data["DefeatedSwampShaman"] == true ? false : true;
+    }
+    else if (key == "voltwyrm") {
+        player_data["defeatedZapCoreEnemy"] = player_data["defeatedZapCoreEnemy"] == true ? false : true;
+    }
+    else if (key == "conchfly") {
+        player_data["defeatedCoralDrillerSolo"] = player_data["defeatedCoralDrillerSolo"] == true ? false : true;
+    }
+    else if (key == "first_sinner") {
+        player_data["defeatedFirstWeaver"] = player_data["defeatedFirstWeaver"] == true ? false : true;
+    }
+    else if (key == "broodmother") {
+        player_data["defeatedBroodMother"] = player_data["defeatedBroodMother"] == true ? false : true;
+    }
+    else if (key == "trobbio") {
+        player_data["defeatedTrobbio"] = player_data["defeatedTrobbio"] == true ? false : true;
+    }
+    else if (key == "cogwork_dancers") {
+        player_data["defeatedCogworkDancers"] = player_data["defeatedCogworkDancers"] == true ? false : true;
+    }
+    else if (key == "lace2") {
+        player_data["defeatedLaceTower"] = player_data["defeatedLaceTower"] == true ? false : true;
+    }
+    else if (key == "unravelled") {
+        player_data["wardBossDefeated"] = player_data["wardBossDefeated"] == true ? false : true;
+    }
+    else if (key == "sentinel") {
+        player_data["cog7_automaton_defeated"] = player_data["cog7_automaton_defeated"] == true ? false : true;
+    }
+    else if (key == "garmond_zaza") {
+        const entry = player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Garmond_Zaza");
+        if (entry != null) {
+            player_data["EnemyJournalKillData"]["list"].splice(player_data["EnemyJournalKillData"]["list"].indexOf(entry), 1);
+        }
+        else {
+            const jsonEntry = {
+                Name: "Garmond_Zaza",
+                Record: {
+                    Kills: 1,
+                    HasBeenSeen: true
+                },
+            };
+            player_data["EnemyJournalKillData"]["list"].push(jsonEntry);
+        }
+    }
+    //#endregion
+    //#region Act3
+    else if (key == "bell_eater") {
+        const entry = player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Giant Centipede");
+        if (entry != null) {
+            player_data["EnemyJournalKillData"]["list"].splice(player_data["EnemyJournalKillData"]["list"].indexOf(entry), 1);
+        }
+        else {
+            const jsonEntry = {
+                Name: "Giant Centipede",
+                Record: {
+                    Kills: 1,
+                    HasBeenSeen: true
+                },
+            };
+            player_data["EnemyJournalKillData"]["list"].push(jsonEntry);
+        }
+    }
+    else if (key == "moss_mother3") {
+        record = player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Mossbone Mother")["Record"]
+        if (record["Kills"] == 2) {
+            record["Kills"] = 1;
+        }
+        else if (record["Kills"] == 4) {
+            record["Kills"] = 3;
+        }
+        else if (record["Kills"] == 1) {
+            record["Kills"] = 2;
+        }
+        else if (record["Kills"] == 3) {
+            record["Kills"] = 4;
+        }
+    }
+    else if (key == "karmelita") {
+        player_data["defeatedAntQueen"] = player_data["defeatedAntQueen"] == true ? false : true;
+    }
+    else if (key == "gurr") {
+        player_data["defeatedAntTrapper"] = player_data["defeatedAntTrapper"] == true ? false : true;
+    }
+    else if (key == "crawfather") {
+        player_data["defeatedCrowCourt"] = player_data["defeatedCrowCourt"] == true ? false : true;
+    }
+    else if (key == "seth") {
+        player_data["defeatedSeth"] = player_data["defeatedSeth"] == true ? false : true;
+    }
+    else if (key == "nyleth") {
+        player_data["defeatedFlowerQueen"] = player_data["defeatedFlowerQueen"] == true ? false : true;
+    }
+    else if (key == "khann") {
+        player_data["defeatedCoralKing"] = player_data["defeatedCoralKing"] == true ? false : true;
+    }
+    else if (key == "watcher_at_the_edge") {
+        player_data["defeatedGreyWarrior"] = player_data["defeatedGreyWarrior"] == true ? false : true;
+    }
+    else if (key == "palestag") {
+        player_data["defeatedWhiteCloverstag"] = player_data["defeatedWhiteCloverstag"] == true ? false : true;
+    }
+    else if (key == "clover_dancers") {
+        player_data["defeatedCloverDancers"] = player_data["defeatedCloverDancers"] == true ? false : true;
+    }
+    else if (key == "pinstress") {
+        player_data["defeatedSongChevalierBoss"] = player_data["defeatedSongChevalierBoss"] == true ? false : true;
+    }
+    else if (key == "zango") {
+        player_data["BlueScientistDead"] = player_data["BlueScientistDead"] == true ? false : true;
+    }
+    else if (key == "tormented_trobbio") {
+        player_data["defeatedTormentedTrobbio"] = player_data["defeatedTormentedTrobbio"] == true ? false : true;
+    }
+    else if (key == "lost_garmond") {
+        player_data["garmondBlackThreadDefeated"] = player_data["garmondBlackThreadDefeated"] == true ? false : true;
+    }
+    else if (key == "lost_lace") {
+        const entry = player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Lost Lace");
+        if (entry != null) {
+            player_data["EnemyJournalKillData"]["list"].splice(player_data["EnemyJournalKillData"]["list"].indexOf(entry), 1);
+        }
+        else {
+            const jsonEntry = {
+                Name: "Lost Lace",
+                Record: {
+                    Kills: 1,
+                    HasBeenSeen: true
+                },
+            };
+            player_data["EnemyJournalKillData"]["list"].push(jsonEntry);
+        }
+    }
+    else if (key == "summoned_saviour") {
+        const entry = player_data["EnemyJournalKillData"]["list"].find(item => item["Name"] == "Abyss Mass");
+        if (entry != null) {
+            player_data["EnemyJournalKillData"]["list"].splice(player_data["EnemyJournalKillData"]["list"].indexOf(entry), 1);
+        }
+        else {
+            const jsonEntry = {
+                Name: "Abyss Mass",
+                Record: {
+                    Kills: 1,
+                    HasBeenSeen: true
+                },
+            };
+            player_data["EnemyJournalKillData"]["list"].push(jsonEntry);
+        }
+    }
+    //#endregion
+
+    console.log(player_data);
+    toggle_ability(obj);
+}
+
 function switchTab(btn, path, reload_id) {
     document.querySelectorAll(".tab-button").forEach(b => b.classList.remove("tab-button-selected"));
     btn.classList.add("tab-button-selected");
@@ -455,6 +732,7 @@ function switchTab(btn, path, reload_id) {
 
         if (reload_id == "items") update_items();
         else if (reload_id == "abilities") update_abilities();
+        else if (reload_id == "bosses") update_bosses();
     });
 }
 
